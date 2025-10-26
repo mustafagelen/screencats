d81de73430573ff4542821375d307acfde0f8741
Mustafa GELEN <73999126+mustafagelen@users.noreply.github.com>
2025-10-26 16:26:00 +0300

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-
+const nextConfig: NextConfig & {
+  eslint?: {
+    ignoreDuringBuilds?: boolean;
+  };
+} = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
